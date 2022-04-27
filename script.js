a3867ce59d70a2ee56ae153761b5fbe2d1bd3d57
ayyybubu <wrkbefore+git@gmail.com>
2022-04-27 14:45:55 +0200

    test

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    navigator.clipboard.writeText("ayyybubu#4050");
+    
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied!";
+  }
+  
+  function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+  }
