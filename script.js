e6c7f868f004061950768f51e57f57775559361e
ayyybubu <wrkbefore+git@gmail.com>
2022-04-27 17:04:16 +0200

    v2

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ function myFunction() {
     var tooltip = document.getElementById("myTooltip");
     tooltip.innerHTML = "Copy to clipboard";
   }
+
+  function loadBadges() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "badges.txt", true);
+    xhttp.send();
+  }
+
+  function loadEmotes() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "emotes.txt", true);
+    xhttp.send();
+  }
+
+  function loadContact() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "contact.txt", true);
+    xhttp.send();
+  }
